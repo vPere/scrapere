780d1005f685e4cd936f21e51fb6e3c6cec3e370
vpere <pere.vidiella@gmail.com>
2023-05-16 11:39:47 +0200

    missing

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='scrapere',
     packages=find_packages(),
-    version='0.1.6',
+    version='0.1.7',
     description='Small set of my web scraping python tools',
     author='vPere',
     license='MIT',
